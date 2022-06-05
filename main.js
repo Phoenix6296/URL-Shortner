4aef0dcf8be882e2853cf6091d4b4782c273c927
Krishna <krishnabiswakarmasc@gmail.com>
2022-06-05 17:39:10 +0530

    Final Commit

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-var endpoint = "https://www.jsonstore.io/8ba4fd855086288421f770482e372ccb5a05d906269a34da5884f39eed0418a1";
+var endpoint = "https://api.jsonbin.io/v3/b/629c8850449a1f3821fe5af7";
 
 function geturl() {
     var url = document.getElementById("urlinput").value;
@@ -27,32 +27,68 @@ function genhash() {
 }
 
 function send_request(url) {
-    this.url = url;
-    $.ajax({
-        'url': endpoint + "/" + window.location.hash.substr(1),
-        'type': 'POST',
-        'data': JSON.stringify(this.url),
-        'dataType': 'json',
-        'contentType': 'application/json; charset=utf-8'
-    })
+    window.url = url;
+    let hash;
+    fetch(endpoint + '/latest', {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json',
+            'X-Master-key': '$2b$10$UYC5zLo/WSCotxdhCC0ciejNIIDGx1RT326YCHOQq8apEukp5bAxG',
+        }
+    }).then(function (response) {
+        return response.json();
+    }).then(function (data) {
+        console.log(data);
+        window.data = data.record;
+
+
+        if (!window.data[window.url]) {
+            let random = getrandom();
+            window.data[url] = random;
+            window.data[random] = url;
+            fetch(endpoint, {
+                method: 'PUT',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'X-Master-Key': '$2b$10$UYC5zLo/WSCotxdhCC0ciejNIIDGx1RT326YCHOQq8apEukp5bAxG'
+                },
+                body: JSON.stringify(window.data)
+            })
+                .then(res => res.json())
+                .then(json => {
+                    document.getElementById('shortURL').innerHTML = "Short URL is: " + window.location.href + '#' + json.record[url];
+                });
+        }
+        else {
+            document.getElementById('shortURL').innerHTML = "Your short URL is: " + window.location.host + '/#' + window.data[url];
+        }
+    });
 }
 
 function shorturl() {
     var longurl = geturl();
-    genhash();
     send_request(longurl);
 }
 
-var hashh = window.location.hash.substr(1)
-
+var hashh = window.location.hash.substring(1)
+console.log(hashh);
 if (window.location.hash != "") {
-    $.getJSON(endpoint + "/" + hashh, function (data) {
-        data = data["result"];
-
-        if (data != null) {
-            window.location.href = data;
+    fetch(endpoint + '/latest', {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json',
+            'X-Master-Key': '$2b$10$UYC5zLo/WSCotxdhCC0ciejNIIDGx1RT326YCHOQq8apEukp5bAxG'
         }
-
-    });
+    })
+        .then(res => res.json())
+        .then(json => {
+            console.log(json);
+            if (json.record[hashh]) {
+                window.location.href = json.record[hashh];
+            }
+            else {
+                document.getElementById('shortURL').innerHTML = "Invalid URL";
+            }
+        });
 }
 
